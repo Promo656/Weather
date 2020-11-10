b1b36e991aed5b66864da575a81d39b6a387c8d5
Egor <egormarkov1408@gmail.com>
2020-11-10 23:53:29 +0300

    Updates

---
+++ precache-manifest.4849f0bed99721b20cf6b593776ebcfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8751a4122084b5f22c212c2e1c964a52",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "874b99bfb56f21004ac4",
+    "url": "/Weather/static/css/main.54704681.chunk.css"
+  },
+  {
+    "revision": "254554cc6d789781471a",
+    "url": "/Weather/static/js/2.be4a50ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Weather/static/js/2.be4a50ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874b99bfb56f21004ac4",
+    "url": "/Weather/static/js/main.0714911a.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  }
+]);
