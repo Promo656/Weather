ce39ced91ef4fc1d41071147a2d57e77abd43157
Egor <egormarkov1408@gmail.com>
2020-11-27 21:36:33 +0300

    commit

--- src/BLL/Redux/redux-store.tsx
+++ src/BLL/Redux/redux-store.tsx
@@ -2,14 +2,27 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {AppReducer} from "./AppReducer";
 import {WeatherReducer} from "./WeatherReducer";
 import thunkMiddleware from "redux-thunk"
+import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 
-let reducers=combineReducers({
-    app:AppReducer,
-    weather:WeatherReducer
+let reducers = combineReducers({
+    app: AppReducer,
+    weather: WeatherReducer
 })
 
-export const store=createStore(reducers, applyMiddleware(thunkMiddleware))
-export type StateType=ReturnType<typeof reducers>
+const middleware = getDefaultMiddleware({
+    immutableCheck: false,
+    serializableCheck: false,
+    thunk: true,
+})
+
+export const store = configureStore({
+    reducer: {...reducers},
+    middleware,
+    devTools: process.env.NODE_ENV !== "production"
+})
+
+//export const store=createStore(reducers, applyMiddleware(thunkMiddleware))
+export type StateType = ReturnType<typeof reducers>
 
 // @ts-ignore
-window.store=store
+window.store = store
